bccccb38302b2cf6bec320d54f06160b908994d8
Jose Cordero <monocilindro1@gmail.com>
2024-05-26 19:21:12 +0200

    Update kmall2xyz_converter.sh

--- kmall2xyz_converter.sh
+++ kmall2xyz_converter.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+# This bash script requires MBsystem. It generates point clouds as ASCII xyz files (UTM East, UTM West and height) from the existing Kongsberg raw files in the current folder.
+# It has been tested in MBsystem 5.8.2 beta, and Ubuntu 22.04 running in WSL (Win11)
+# Use CloudCompare to visualize the output
 # Find and process kmall files in the script's directory
 
 for file in *.kmall; do
